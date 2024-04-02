2537f4bbdb787524faef26bf846323258b84d5d4
net-tools06 <133276694+net-tools06@users.noreply.github.com>
2024-04-02 00:34:22 +0000

    Ajout du script de lien

---
+++ opencart.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import ttk
+import mysql.connector
+from web import *
+from sales import *
+from clients import *
+from transactions import *
+from catalogues import *
+
+class OpenCartApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("OpenCart Application")
+
+        self.db = mysql.connector.connect(
+            host="localhost",
+            user="patricia",
+            password="passer",
+            database="opencart"
+        )
+
+        # Creation ds onglets (une par option)
+        self.notebook = ttk.Notebook(self.root)
+        self.notebook.pack(pady=10)
+
+        self.web_access_tab = WebAccessTab(self.notebook, self.db)
+        self.block_sales_tab = BlockSalesTab(self.notebook, self.db)
+        self.connected_clients_tab = ConnectedClientsTab(self.notebook, self.db)
+        
+
+    def run(self):
+        self.root.mainloop()
